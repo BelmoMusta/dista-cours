8f2873ff7d8297f8368acf166a42f41d7dc9e2d5
Mustapha Belmokhtar <mbelmokhtar@sqli.com>
2020-12-24 11:05:29 +0100

    kill process

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-kill -9 $(ps ax | grep dista_cours.jar | fgrep -v grep | awk '{ print $1 }')
+PROCESS_ID=$(ps ax | grep dista_cours.jar | fgrep -v grep | awk '{ print $1 }')
+if [[ ! "$PROCESS_ID" ]]
+then
+    kill -9 "$PROCESS_ID"
+fi
 nohup java -jar dista_cours.jar --spring.datasource.username=$DATABASE_USERNAME --spring.datasource.password=$DATABASE_PASSWORD &
