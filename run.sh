793e8ea6b3df7e9fa8ba687d91c954c9e2054da0
Mustapha Belmokhtar <mbelmokhtar@sqli.com>
2020-12-26 00:10:18 +0100

    add relative path to jar

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if [[  "$PROCESS_ID" ]]
 then
     kill -9 "$PROCESS_ID"
 fi
-CURRENT_DIR=$(pwd)
+CURRENT_DIR=$1
 nohup java -jar $CURRENT_DIR/dista_cours.jar --spring.datasource.username=$DATABASE_USERNAME --spring.datasource.password=$DATABASE_PASSWORD &
